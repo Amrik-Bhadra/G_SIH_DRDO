ca19113f22fcd5d838edb4070fa9bac9639d0620
amrik_bhadra <amrik.bhadra@gmail.com>
2024-11-17 18:25:33 +0530

    pages folder updated inside client folder

---
+++ client/src/pages/LoginForm/LoginForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginForm = () => {
+  return (
+    <main className='h-[100vh] w-[100vw] bg-[#eee] flex justify-center items-center'>
+        
+    </main>
+  )
+}
+
+export default LoginForm

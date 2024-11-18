6e16f9140adae9ec0d34f4bb5b8ef9504bf48df2
rbvarun <varunbalbudhe7@gmail.com>
2024-11-18 14:00:09 +0530

    Expert Directory Refinement

--- server/app.js
+++ server/app.js
@@ -1,19 +1,24 @@
-const express = require("express");
 const connectDB = require("./db/connectDb");
-const app = express();
-const expertRoutes = require("./routes/expertRoutes");
+const express = require("express");
 const cookieParser = require("cookie-parser");
+const app = express();
+
+const expertRoutes = require("./routes/expertRoutes");
+const localRoutes = require("./routes/localRoutes");
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/expert", expertRoutes);
+app.use("/api", localRoutes);
+
 const PORT = process.env.PORT || 8000;
 const mongoURI = process.env.MONGO_URI;
 
 const connection = async () => {
   await connectDB(mongoURI);
   app.listen(PORT, () => {
-    console.log("Server & DB is Working fine :-:");
+    console.log("Server Status\t\t { OK } :-:\nDB Status\t\t { OK } :-:");
   });
 };
 

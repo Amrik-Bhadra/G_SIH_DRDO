eef0e148d329b876ed3d4f3c40474360c06a4c77
rbvarun <varunbalbudhe7@gmail.com>
2024-11-20 04:06:27 +0530

    checked page

--- client/src/components/ExpertDashboard/Calender.jsx
+++ client/src/components/ExpertDashboard/Calender.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function Calender() {
   return (
-    <div className="w-full h-full border-2 border-slate-200 shadow-md rounded-2xl">
+    <div className="w-full h-full border-2 pl-5 pr-5 border-slate-200 shadow-md rounded-2xl">
       <div className="w-full h-[20%] flex justify-between items-center p-2">
         <p className="text-xl -tracking-tight font-semibold text-slate-800">
-          Notifications
+          Calender
         </p>
         
       </div>

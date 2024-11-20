c90906121b500065098cce1b8a96ac6e968e4482
Rohan Agrawal <104892210+Rohan452004@users.noreply.github.com>
2024-11-20 15:33:31 +0530

    commits

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,34 +4,31 @@ import LoginForm from './pages/LoginForm/LoginForm';
 import { Toaster } from 'react-hot-toast';
 import ForgotPassword from './pages/LoginForm/ForgotPassword';
 import VerifyOTP from './pages/LoginForm/VerifyOTP';
-import RegistrationForm from './pages/RegistrationForm/CandidateRegistration';
 import ResetPassword from './pages/LoginForm/ResetPassword';
-import RacHeadDashboard from './pages/RacHeadPages/RacHeadDashboard';
-import RacHeadAnalytics from './pages/RacHeadPages/RacHeadAnalytics';
-import RacHeadPannels from './pages/RacHeadPages/RacHeadPannels';
-import CandidateCompleteDetail from './pages/RegistrationForm/CandidateCompleteDetail';
 import ExpertCompleteDetail from './pages/RegistrationForm/ExpertCompleteDetail';
-import ExpertRegistrationForm from './pages/RegistrationForm/ExpertRegistration';
+import RegistrationChoice from './pages/RegistrationForm/RegistrationChoice';
 import ExpertRegistration from './pages/RegistrationForm/ExpertRegistration';
 import CandidateRegistration from './pages/RegistrationForm/CandidateRegistration';
-import RegistrationChoice from './pages/RegistrationForm/RegistrationChoice';
-import TwoFactorAuthentication from './pages/LoginForm/TwoFactorAuthentication';
+import CandidateCompleteDetail from './pages/RegistrationForm/CandidateCompleteDetail';
+import CandidateQuizRedirect from './pages/RegistrationForm/CandidateQuizRedirect';
+import ExpertQuizRedirect from './pages/RegistrationForm/ExpertQuizRedirect';
+import Candidatedashboard from './pages/CandidatePages/Candidatedashboard';
+import ExpertDashboard from './pages/ExpertPages/ExpertDashboard';
 
 const routes = createBrowserRouter([
   { path: '/', element: <LoginForm /> },
   { path: '/forgotPassword', element: <ForgotPassword /> },
   { path: '/verifyOtp', element: <VerifyOTP /> },
-  { path: '/twofactorauthentication', element: <TwoFactorAuthentication/>},
+  { path: '/resetpassword', element: <ResetPassword /> },
   { path: '/registrationchoice', element: <RegistrationChoice /> },
   { path: '/register/candidateregister', element: <CandidateRegistration /> },
   { path: '/register/expertregister', element: <ExpertRegistration /> },
-  { path: '/register/expert', element: <ExpertRegistrationForm /> },
   { path: '/register/candidatecompletedetail', element: <CandidateCompleteDetail/> },
   { path: '/register/expertcompletedetail', element: <ExpertCompleteDetail/> },
-  { path: '/resetpassword', element: <ResetPassword /> },
-  { path: '/rachead/', element: <RacHeadDashboard/> },
-  { path: '/rachead/analytics', element: <RacHeadAnalytics/> },
-  { path: '/rachead/pannels', element: <RacHeadPannels/> },
+  { path: '/register/candidate/quiz', element: <CandidateQuizRedirect /> },
+  { path: '/register/expert/quiz', element: <ExpertQuizRedirect /> },
+  { path: '/candidate/dashboard', element: <Candidatedashboard /> },
+  { path: '/expert/dashboard', element: <ExpertDashboard /> },
 ]);
 
 
